476dd4d316f2c714c0ecee4244086530cf6f92fe
beeb <vbersier@gmail.com>
2023-03-20 19:54:01 +0100

    don't try to build, use pre-built library

--- build.rs
+++ build.rs
@@ -1,21 +1,11 @@
-use std::{fs, io::Result};
-
-fn main() -> Result<()> {
-    let occt_paths = fs::read_dir("./occt")?;
-
-    let dirs = occt_paths
-        .into_iter()
-        .filter_map(|i| i.ok().map(|i| i.path()))
-        .filter(|i| i.is_dir());
-
+fn main() {
     cxx_build::bridge("src/main.rs")
         .file("src/OCCTWrapper.cpp")
-        .includes(dirs)
+        .include("/home/valentin/.nix-profile/include/opencascade")
         .flag_if_supported("-std=c++14")
         .compile("occtwrapper");
 
     println!("cargo:rerun-if-changed=src/main.rs");
     println!("cargo:rerun-if-changed=src/OCCTWrapper.cpp");
     println!("cargo:rerun-if-changed=src/OCCTWrapper.hpp");
-    Ok(())
 }
