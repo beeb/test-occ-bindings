4885c074d8b940e86ab5744ff8bd2d5d749c1de1
beeb <vbersier@gmail.com>
2023-03-20 21:01:42 +0100

    trying to add the required libraries (actually some are not required)

--- build.rs
+++ build.rs
@@ -1,5 +1,17 @@
 fn main() {
     println!("cargo:rustc-link-search=/home/valentin/.nix-profile/lib");
+    println!("cargo:rustc-link-lib=TKernel");
+    println!("cargo:rustc-link-lib=TKMath");
+    println!("cargo:rustc-link-lib=TKBRep");
+    println!("cargo:rustc-link-lib=TKXSBase");
+    println!("cargo:rustc-link-lib=TKService");
+    println!("cargo:rustc-link-lib=TKV3d");
+    println!("cargo:rustc-link-lib=TKOpenGl");
+    println!("cargo:rustc-link-lib=TKIGES");
+    println!("cargo:rustc-link-lib=TKSTEP");
+    println!("cargo:rustc-link-lib=TKSTL");
+    println!("cargo:rustc-link-lib=TKVRML");
+    println!("cargo:rustc-link-lib=TKLCAF");
 
     cxx_build::bridge("src/main.rs")
         .file("src/OCCTWrapper.cpp")
