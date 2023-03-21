9ad050497e97aa3102b0290ef7a1dab2c53ac72e
Valentin Bersier <vbersier@gmail.com>
2023-03-21 16:31:05 +0100

    add windows config

--- build.rs
+++ build.rs
@@ -1,27 +1,28 @@
+use std::path::Path;
+
 fn main() {
     let home_dir = home::home_dir().unwrap();
+    let mut lib_path = home_dir.join(".nix-profile/lib");
+    let mut include_path = home_dir.join(".nix-profile/include/opencascade");
+    if cfg!(windows) {
+        lib_path = Path::new("C:\\OpenCASCADE-7.7.0-vc14-64\\opencascade-7.7.0\\win64\\vc14\\lib")
+            .to_path_buf();
+        include_path =
+            Path::new("C:\\OpenCASCADE-7.7.0-vc14-64\\opencascade-7.7.0\\inc").to_path_buf();
+    }
 
-    println!(
-        "cargo:rustc-link-search={}",
-        home_dir.join(".nix-profile/lib").to_string_lossy()
-    );
+    println!("cargo:rustc-link-search={}", lib_path.to_string_lossy());
     println!("cargo:rustc-link-lib=TKernel");
     println!("cargo:rustc-link-lib=TKMath");
     println!("cargo:rustc-link-lib=TKBRep");
     println!("cargo:rustc-link-lib=TKXSBase");
-    println!("cargo:rustc-link-lib=TKService");
-    println!("cargo:rustc-link-lib=TKV3d");
-    println!("cargo:rustc-link-lib=TKOpenGl");
-    println!("cargo:rustc-link-lib=TKIGES");
     println!("cargo:rustc-link-lib=TKSTEP");
     println!("cargo:rustc-link-lib=TKSTL");
-    println!("cargo:rustc-link-lib=TKVRML");
-    println!("cargo:rustc-link-lib=TKLCAF");
     println!("cargo:rustc-link-lib=TKMesh");
 
     cxx_build::bridge("src/main.rs")
         .file("src/OCCTWrapper.cpp")
-        .include(home_dir.join(".nix-profile/include/opencascade"))
+        .include(include_path)
         .include("src")
         .flag_if_supported("-std=c++14")
         .compile("occtwrapper");
